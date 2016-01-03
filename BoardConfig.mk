f76b165a3caf467fb870b5f1370baf8c73d69446
ShevT <shev_t@mail.ru>
2016-01-03 13:04:50 +0300

    lt03xx: clean tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,11 +17,6 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a15
 
-# Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60
-
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -36,36 +31,30 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2524971008
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12863913984
 BOARD_FLASH_BLOCK_SIZE := 4096
 
-# USB Mounting
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
-
-# Recovery:Start
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/lt033g/custombootimg.mk
-
-# Use this flag if the board has a ext4 partition larger than 2gb
+# Filesystem
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 
-# TWRP specific build flags
-TW_NEW_ION_HEAP := true
-TW_THEME := landscape_hdpi
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
+# USB Mounting
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
+TW_MTP_DEVICE := /dev/mtp_usb
+
+# Graphics and Display
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TW_BRIGHTNESS_PATH := /sys/class/backlight/panel/brightness
 TW_MAX_BRIGHTNESS := 250
-TW_NO_REBOOT_BOOTLOADER := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_CRYPTO_FS_TYPE := "ext4"
+TW_THEME := landscape_hdpi
+TW_NEW_ION_HEAP := true
+
+# Crypto
 TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p21"
 TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,discard,journal_async_commit,errors=panic"
-TW_CRYPTO_FS_FLAGS := "0x00000406"
 TW_CRYPTO_KEY_LOC := "footer"
+
+# Storages
 TW_INTERNAL_STORAGE_PATH := "/sdcard"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
@@ -74,4 +63,11 @@ TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_HAS_DUAL_STORAGE :=true
 TW_FLASH_FROM_STORAGE := true
 TW_SDEXT_NO_EXT4 := true
-TW_MTP_DEVICE := /dev/mtp_usb
+
+# No partitioning SD Card
+BOARD_HAS_NO_REAL_SDCARD := true
+
+# TWRP specific build flags
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
